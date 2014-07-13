939e249db7f78288d01f66d3764ac4b98034d059
oxfordbio <oxfordbio@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-07-13 13:38:22 +0000

    cf7 form file
    git-svn-id: https://plugins.svn.wordpress.org/nomorecaptchas/trunk@947742 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ no-more-captchas-cf7.php
@@ -0,0 +1,31 @@
+<?php
+/*
+NoMoreCaptchas
+Oxford BioChronometrics SA
+Version: 1.2.6
+Purpose: Code handler for Contact Form Seven
+*/
+
+
+function xb_nmc_wp_ct7_entry_point($WPCF7_ContactForm){
+
+	$xb_data = $WPCF7_ContactForm->posted_data;
+	if(isset($xb_data['oxbioxid'])){			
+		$xb_state = xb_nmc_maybe_human($xb_data['oxbioxid'],'ct71','ct71');
+		if($xb_state != 1){
+			$WPCF7_ContactForm->skip_mail = true;
+		}else{
+			$WPCF7_ContactForm->mail['subject'] .= " [Validated Not Spam by NoMoreCaptchas]";
+			$WPCF7_ContactForm->mail['body'] .= "\r\n\r\n";
+			$WPCF7_ContactForm->mail['body'] .= "\r\n================================================\r\n";
+			$WPCF7_ContactForm->mail['body'] .= "\r\n NoMoreCaptchas Validated this post as Not Spam\r\n";
+			$WPCF7_ContactForm->mail['body'] .= "\r\n================================================\r\n";
+			$WPCF7_ContactForm->mail['body'] .= "\r\n\r\n";
+		}
+	}else{
+		$WPCF7_ContactForm->skip_mail = true;
+		xb_nmc_definitely_bot('ct72','ct72');
+	}
+}
+
+?>
