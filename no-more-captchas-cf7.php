83efef4b7294d7cce353a89c13b6848d4ba5228b
oxfordbio <oxfordbio@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-07-16 14:40:00 +0000

    Fixed minor glitch in email
    git-svn-id: https://plugins.svn.wordpress.org/nomorecaptchas/trunk@949678 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- no-more-captchas-cf7.php
+++ no-more-captchas-cf7.php
@@ -17,9 +17,9 @@ function xb_nmc_wp_ct7_entry_point($WPCF7_ContactForm){
 		}else{
 			$WPCF7_ContactForm->mail['subject'] .= " [Validated Not Spam by NoMoreCaptchas]";
 			$WPCF7_ContactForm->mail['body'] .= "\r\n\r\n";
-			$WPCF7_ContactForm->mail['body'] .= "\r\n================================================\r\n";
-			$WPCF7_ContactForm->mail['body'] .= "\r\n NoMoreCaptchas Validated this post as Not Spam\r\n";
-			$WPCF7_ContactForm->mail['body'] .= "\r\n================================================\r\n";
+			$WPCF7_ContactForm->mail['body'] .= "\r\n==================================================\r\n";
+			$WPCF7_ContactForm->mail['body'] .= "\r\n NoMoreCaptchas Validated this e-mail as Not Spam \r\n";
+			$WPCF7_ContactForm->mail['body'] .= "\r\n==================================================\r\n";
 			$WPCF7_ContactForm->mail['body'] .= "\r\n\r\n";
 		}
 	}else{
